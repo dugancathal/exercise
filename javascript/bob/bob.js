3bce7bb3490bbf464d878a71682bdac97bea8e93
TJ Taylor <ttaylor@tendrilinc.com>
2013-08-22 18:44:22 -0600

    First iteration of Bob ... kinda OO. We'll see what people think

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,5 +1,30 @@
-var Bob = function() {}; 
+var Bob = function() {};
+
+String.prototype.isQuestion = function() {
+  return this.indexOf('?', this.length - 1) !== -1;
+}
+
+String.prototype.isYelling = function() {
+  return this.toUpperCase() == this; 
+}
+
+String.prototype.isSilent = function() {
+  return this.trim().length === 0;
+}
+
 Bob.prototype.hey = function(statement) {
-  return 'Whatever';
+  if(statement.isSilent()) {
+    return 'Fine. Be that way!';
+  }
+  else if(statement.isYelling()) {
+    return 'Woah, chill out!';
+  }
+  else if(statement.isQuestion()) {
+    return 'Sure.';
+  }
+  else {
+    return 'Whatever.';
+  }
 };
+
 module.exports = Bob;
