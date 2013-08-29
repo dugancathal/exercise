40bc15fa9012147742941f7e8a74049b01f4a957
TJ Taylor <ttaylor@tendrilinc.com>
2013-08-28 21:43:17 -0600

    Ruby: Word count Iteration 2

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -14,10 +14,6 @@ class Phrase
   private
 
   def words
-    normalized_statement_without_punctuation.split(/\s+/)
-  end
-
-  def normalized_statement_without_punctuation
-    statement.downcase.gsub(/[^\sa-zA-Z0-9]/, ' ')
+    statement.downcase.scan /\w+/
   end
 end
