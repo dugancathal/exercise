e84ef783b00ac7af23978fe7b919bea43d5e3443
TJ Taylor <ttaylor@tendrilinc.com>
2013-09-11 21:27:20 -0600

    Ruby: Point Mutation Iteration 2

--- ruby/point-mutations/dna.rb
+++ ruby/point-mutations/dna.rb
@@ -1,12 +1,10 @@
 class DNA < Struct.new(:sequence)
   def hamming_distance(other)
     other_sequence = DNA.new(other).nucleotides
-    distance = 0
-    other_sequence.zip(nucleotides).each do |a, b|
-      break unless a && b
-      distance += 1 if a != b
+    similar_nucleotides = other_sequence.zip(nucleotides).select do |a, b|
+      a && b && a != b
     end
-    distance
+    similar_nucleotides.length
   end
 
   def nucleotides
